0703372099fa19d4613061affcf1b9f0d660a363
Irving J S Linares <irjasali@gmail.com>
2022-06-30 20:57:26 -0500

    Primer Commit creación de la Barra de Navegación

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import logo from './logo.svg'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import { Navbar } from 'react-bootstrap'
+import Navbarcomp from './components/Navbar/Navbarcomp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+      <Navbarcomp>  
+         
+      </Navbarcomp>
+      <div className='content'>
+      
+
+      </div>
+
     </div>
   );
 }
---
+++ src/components/ItemsListCart/ItemsListCart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemsListCart = ({counter}) => {
+  return (
+        <div  style={{color: 'white', fontSize: '20px', marginLeft:'15px'}} >
+           {counter}
+        </div>
+  )
+}
+
+export default ItemsListCart
